80d955d17bd974400cc6cf6463924028fc8b9c3a
xyl0o <xyl0o@posteo.de>
2018-12-03 21:37:59 +0100

    Working 03 part 1 (not optimized

--- 03/slice.py
+++ 03/slice.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 import numpy as np
 
 from typing import Tuple, List
@@ -8,7 +10,7 @@ Claim = Tuple[int, int, int, int, int]
 Shape = Tuple[int, int]
 
 
-def split(line: str) -> Claim:
+def parse_lines(line: str) -> Claim:
     num, _, coord, size = line.split()
     left, right = coord.split(',')
     width, height = size.split('x')
@@ -35,21 +37,15 @@ def claim_to_field(claim: Claim, shape: Shape) -> np.ndarray:
 
 if __name__ == '__main__':
 
-    # with open(sys.argv[1]) as f:
-    #     lines = f.read().splitlines()
+    with open(sys.argv[1]) as f:
+        lines = f.read().splitlines()
 
-    # claims = [split(c) for c in lines]
+    claims = [parse_lines(c) for c in lines]
 
-    claims = [
-        (0, 1, 1, 2, 1),
-        (0, 0, 0, 2, 2),
-        (0, 3, 3, 1, 1),
-        (0, 0, 1, 7, 4),
-    ]
     idx_claims = [size_to_indicies(c) for c in claims]
     shape = max_shape(idx_claims)
     fields = [claim_to_field(claim, shape=shape) for claim in idx_claims]
     field_array = np.stack(fields, axis=0)
     sum_array = np.sum(field_array, axis=0)
 
-    print(sum_array.T)
+    print(f'Square inches with >= 2 claims: {np.sum(sum_array >= 2)}')
