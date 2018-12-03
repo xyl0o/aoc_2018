3d83a228f6a4f1ffc221795dc4b9689a44752b9e
xyl0o <xyl0o@posteo.de>
2018-12-04 00:28:19 +0100

    Do not do complicated array resizing, find out shape beforehand (03_part1)

--- 03/slice.py
+++ 03/slice.py
@@ -4,13 +4,15 @@ import sys
 
 import numpy as np
 
-from typing import Tuple, List
+from functools import reduce
+
+from typing import Tuple
 
 Claim = Tuple[int, int, int, int, int]
 Shape = Tuple[int, int]
 
 
-def parse_lines(line: str) -> Claim:
+def parse_claim(line: str) -> Claim:
     num, _, coord, size = line.split()
     left, right = coord.split(',')
     width, height = size.split('x')
@@ -18,34 +20,19 @@ def parse_lines(line: str) -> Claim:
     return int(num[1:]), int(left), int(right[:-1]), int(width), int(height)
 
 
-def size_to_indicies(claim: Claim) -> Claim:
-    return (
-        claim[0], claim[1], claim[2], claim[1] + claim[3], claim[2] + claim[4])
-
-
-def claim_to_field(claim: Claim) -> np.ndarray:
-    arr = np.zeros(shape=(claim[3], claim[4]), dtype=np.int16)
-    arr[claim[1]:claim[3], claim[2]:claim[4]] = 1
-    return arr
-
-
 if __name__ == '__main__':
 
     with open(sys.argv[1]) as f:
-        lines = f.read().splitlines()
+        claims = [parse_claim(line) for line in f.read().splitlines()]
 
-    sum_array = np.ndarray(shape=(0, 0), dtype=np.int16)
+    shape = reduce(
+        lambda agg, e: (max(agg[0], e[1] + e[3]), max(agg[1], e[2] + e[4])),
+        claims,
+        (0, 0))
 
-    for line in lines:
-        claim = parse_lines(line)
-        idx_claim = size_to_indicies(claim)
-        field = claim_to_field(idx_claim)
-        zero_array = np.zeros(shape=(
-            max(sum_array.shape[0], field.shape[0]),
-            max(sum_array.shape[1], field.shape[1])))
-        zero_array[:sum_array.shape[0], :sum_array.shape[1]] += sum_array
-        zero_array[:field.shape[0], :field.shape[1]] += field
+    sum_array = np.zeros(shape=shape, dtype=np.int16)
 
-        sum_array = zero_array
+    for _, x, y, w, h in claims:
+        sum_array[x:x + w, y: y + h] += 1
 
     print(f'Square inches with >= 2 claims: {np.sum(sum_array >= 2)}')
