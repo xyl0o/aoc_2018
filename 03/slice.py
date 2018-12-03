8fb17fa01297b865a800f4b81a0fe5f32e84d9dd
xyl0o <xyl0o@posteo.de>
2018-12-03 21:58:34 +0100

    Add claims sequentially (03_part1)

--- 03/slice.py
+++ 03/slice.py
@@ -23,14 +23,8 @@ def size_to_indicies(claim: Claim) -> Claim:
         claim[0], claim[1], claim[2], claim[1] + claim[3], claim[2] + claim[4])
 
 
-def max_shape(claims: List[Claim]) -> Shape:
-    return (
-        max(claims, key=lambda x: x[3])[3],
-        max(claims, key=lambda x: x[4])[4])
-
-
-def claim_to_field(claim: Claim, shape: Shape) -> np.ndarray:
-    arr = np.zeros(shape=shape, dtype=np.int16)
+def claim_to_field(claim: Claim) -> np.ndarray:
+    arr = np.zeros(shape=(claim[3], claim[4]), dtype=np.int16)
     arr[claim[1]:claim[3], claim[2]:claim[4]] = 1
     return arr
 
@@ -40,12 +34,18 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         lines = f.read().splitlines()
 
-    claims = [parse_lines(c) for c in lines]
+    sum_array = np.ndarray(shape=(0, 0), dtype=np.int16)
 
-    idx_claims = [size_to_indicies(c) for c in claims]
-    shape = max_shape(idx_claims)
-    fields = [claim_to_field(claim, shape=shape) for claim in idx_claims]
-    field_array = np.stack(fields, axis=0)
-    sum_array = np.sum(field_array, axis=0)
+    for line in lines:
+        claim = parse_lines(line)
+        idx_claim = size_to_indicies(claim)
+        field = claim_to_field(idx_claim)
+        zero_array = np.zeros(shape=(
+            max(sum_array.shape[0], field.shape[0]),
+            max(sum_array.shape[1], field.shape[1])))
+        zero_array[:sum_array.shape[0], :sum_array.shape[1]] += sum_array
+        zero_array[:field.shape[0], :field.shape[1]] += field
+
+        sum_array = zero_array
 
     print(f'Square inches with >= 2 claims: {np.sum(sum_array >= 2)}')
